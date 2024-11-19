8630c71be6ec23f657800f6ad8248cce9d22c363
yeonho <kstory8715@gmail.com>
2024-11-19 19:37:05 +0900

    1. 프로젝트 구조 변경 2. 루트 URL에대한 메인 리다이렉트 처리 추가

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,25 +1,5 @@
-'use client';
+import { redirect } from '@/i18n/routing'
 
-import Image from 'next/image';
-import styles from './page.module.css';
-import { useRouter } from '@/i18n/routing';
-
-export default function Home() {
-  const router = useRouter();
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src='/next.svg'
-          alt='Next.js logo'
-          width={180}
-          height={38}
-          priority
-        />
-        <div className={styles.ctas}></div>
-        <button onClick={() => router.push('/user')}>Post</button>
-      </main>
-    </div>
-  );
+export default function Web() {
+  redirect({ href: '/main', locale: 'ko' })
 }
